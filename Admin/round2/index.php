2ffd04624d25450e69a756faca502e7b1789e446
Johnexzy <obajohn75@gmail.com>
2019-11-11 09:01:17 +0100

    Modified

--- Admin/round2/index.php
+++ Admin/round2/index.php
@@ -18,7 +18,12 @@
         <![endif]-->
         <h3>
             "     Some Questions Might require images for illustrations, especially in subjects/courses related to science<br /> To enable simplicity when using this images, they're required to be uploaded, then you only make use of the image links. <br> With that you can place them at any position of your choice. please kindly watch the video provided in the manual, or read the documentation fully."
+            
         </h3>
+        <h2>
+            HOW TO ADD QUESTIONS
+        </h2>
+        <p>For Question that require images, you'll need to write these questions with MS WORD, convert then to a PDF file. Here you can cut out the questions in the form of images, and then get them uploaded.</p>
         <script src="" async defer></script>
     </body>
 </html>
