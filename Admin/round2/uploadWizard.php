6aab59e63a29bfee8a6fb106a3be4958b6dff14b
Johnexzy <obajohn75@gmail.com>
2019-11-11 12:06:29 +0100

    modified

---
+++ Admin/round2/uploadWizard.php
@@ -0,0 +1,37 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>UPLOAD WIZARD</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+    </head>
+    <body>
+    <div class="container">
+    <h3 class="h2" style="text-align:center">
+    UPLOAD WIZARD
+    </h3>
+        <section style="width:80%;height:400px; border:2px dashed; border-radius:23px;margin: auto">
+            <div class="card" style="background:#88888875; width:45%; height:200px; margin:60px auto; text-align:center;box-shadow:1px 1px 9px 2px;border-radius:20px">
+            <p>CLICK HERE TO UPLOAD IMAGES</p>
+                <span class="glyphicon glyphicon-upload" style="margin-top:50px; font-size:24px;color:blue"></span>
+            </div>
+            <input type="file" name="upload" id="upload">
+        </section>
+    </div>
+        
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        
+        <script src=""></script>
+    </body>
+</html>
